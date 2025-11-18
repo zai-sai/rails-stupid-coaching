0a50f2af89d04782e17747ba33094cc224cf8a24
Zainab Saigol <z.s.saigol@hotmail.com>
2025-11-18 12:16:37 +0000

    added styling to all pages, moved the ask flow to the root page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get "ask", to: "questions#ask"
+  root to: "questions#ask"
   get "answer", to: "questions#answer"
 end
